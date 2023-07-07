6c3eca47d7775b395342728ab6a2dbfeaf748436
Amit Baid <baidstar@hotmail.com>
2023-07-07 17:52:03 -0600

    add styling

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center">AI-Powered Prompts</span>
       </hi>
-      <p>
+      <p className="desc text-center">
         Promptopia is an open-souce AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
