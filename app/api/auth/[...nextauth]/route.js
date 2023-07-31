765bb10a0eb6d87c52565426e2b477a4f1f53701
Amit Baid <baidstar@hotmail.com>
2023-07-31 10:32:31 -0600

    add nextauth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: '',
+      clientSecret: '',
+    }),
+  ],
+  async sessions({ session }) {},
+  async signIn({ profile }) {},
+});
+
+export { handler as GET, handler as POST };
